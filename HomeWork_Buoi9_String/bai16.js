5c431551d51ede9285e2022fa741e9ed6ddd0cb2
Quang Tiến <56441644+tranquangtien1996@users.noreply.github.com>
2022-09-15 08:17:52 +0700

    bai16

--- HomeWork_Buoi9_String/bai16.js
+++ HomeWork_Buoi9_String/bai16.js
@@ -1,2 +1,16 @@
 // 16. Viết chương trình lấy  id name của fb.
 // VD:https://www.facebook.com/tung.nguyenthac.1/, output: “tung nguyenthac”
+
+let a = "https://www.facebook.com/tung.nguyenthac.1/".split('/');
+let b = a[a.length -2].split('.');
+let c = b.slice(0, b.length - 1).join(' ');
+console.log(c);
+
+const getIDFacebook = input => {
+    let a = input.split('/');
+    let b = a[a.length - 2].split('.');
+    let c = b.slice(0, b.length - 1).join(' ');
+    return c;
+}
+
+console.log(getIDFacebook('https://www.facebook.com/tien.tranquang.547/'));
